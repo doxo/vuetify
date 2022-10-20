e68eda1ec9e87328148758f5397814e396a9cdea
Steve Walsh <steve.walsh@doxo.com>
2022-10-20 20:27:03 +0000

    modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 # npm config set //npm.pkg.github.com/:_authToken ${NPM_API_KEY:?}\
 # npm whoami
-yarn lerna publish from-git --npm-tag v3.0.0-beta.10 --yes --no-verify-access
+yarn lerna publish from-git --npm-tag v3.0.0-beta.10b --yes --no-verify-access
